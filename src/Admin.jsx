9bd2a4e379e601cec1e3aa24a4e584a87972d2db
mattia <mattia.asti@gmail.com>
2020-05-13 00:39:08 +0100

    WIP

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useSWR from 'swr';
-import { Flex, Text, Button, Box } from 'rebass';
-import { Input, Label } from '@rebass/forms';
+import { Flex, Text, Box } from 'rebass';
 import request from 'superagent';
 import { useAuth0 } from './services/auth0Wrapper';
 import SubscriptionType from './components/SubscriptionType';
 import { BASE_URL } from './config';
+import AddRestaurant from './components/AddRestaurant';
 
 function Admin() {
   const { getTokenSilently } = useAuth0();
-  const [restaurantName, setRestaurantName] = useState('');
-  const { data, error, mutate } = useSWR('/api/restaurants', () =>
+  const { data, error } = useSWR('/restaurants', () =>
     getTokenSilently()
       .then((token) => {
         return request
@@ -25,65 +24,35 @@ function Admin() {
   return (
     <Flex py={2} height="100%" bg="#fff" px={3} flexDirection="column">
       <Box mb={3}>
-        <Text mb={3} textAlign="center" fontSize={4}>
-          Account
-        </Text>
         <SubscriptionType
           type={data.subscriptionType}
           subscriptionEnds={data.subscriptionEnds}
           createdAt={data.createdAt}
         />
       </Box>
-      <Box height={1} bg="#f2f2f2" my={3} />
+      <Box height={1} bg="#f2f2f2" my={2} />
       <Box>
-        <Text textAlign="center" mb={3} fontSize={4}>
-          Ristoranti e menù
-        </Text>
-        {data.Restaurants.map((restaurant) => (
-          <Text key={restaurant.id}>{restaurant.name}</Text>
-        ))}
-        <Box
-          sx={{
-            borderRadius: 4,
-            boxShadow:
-              '0 0.938em 2.188em rgba(50,50,93,.1), 0 0.313em 0.938em rgba(0,0,0,.07)',
-          }}
-          bg="#fff"
-          p={3}
-          maxWidth={500}
-          mx="auto"
-        >
-          <Label mb={1} htmlFor="restaurantName">
-            Nome
-          </Label>
-          <Input
-            id="restaurantName"
-            name="namrestaurantNamee"
-            value={restaurantName}
-            onChange={(e) => setRestaurantName(e.target.value)}
-          />
-          <Button
-            my={3}
-            variant="primary"
-            onClick={async () => {
-              try {
-                const token = await getTokenSilently();
-                await request
-                  .post(`${BASE_URL}/restaurants`)
-                  .set('Authorization', `Bearer ${token}`)
-                  .send({
-                    name: restaurantName,
-                  });
-                mutate();
-                setRestaurantName('');
-              } catch (err) {
-                alert(err);
-              }
-            }}
-          >
-            Aggiungi nuovo ristorante
-          </Button>
-        </Box>
+        <Flex flexDirection="column" justifyContent="center" mb={4}>
+          {data.Restaurants.map((restaurant) => (
+            <Box
+              sx={{
+                borderRadius: 4,
+                boxShadow:
+                  '0 0.938em 2.188em rgba(50,50,93,.1), 0 0.313em 0.938em rgba(0,0,0,.07)',
+              }}
+              bg="#fff"
+              p={3}
+              my={3}
+            >
+              <Flex>
+                <Text fontSize={3} key={restaurant.id}>
+                  {restaurant.name}
+                </Text>
+              </Flex>
+            </Box>
+          ))}
+        </Flex>
+        <AddRestaurant />
       </Box>
       <Text color="red">{JSON.stringify(error)}</Text>
     </Flex>
---
+++ src/components/AddRestaurant.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Box, Flex, Button } from 'rebass';
+import { Label, Input } from '@rebass/forms';
+import { useAuth0 } from '../services/auth0Wrapper';
+import { mutate } from 'swr';
+import { BASE_URL } from '../config';
+import request from 'superagent';
+
+function AddRestaurant({ createRestaurant }) {
+  const { getTokenSilently } = useAuth0();
+  const [restaurantName, setRestaurantName] = useState('');
+  return (
+    <Box
+      sx={{
+        borderRadius: 4,
+        boxShadow:
+          '0 0.938em 2.188em rgba(50,50,93,.1), 0 0.313em 0.938em rgba(0,0,0,.07)',
+      }}
+      bg="#fff"
+      p={3}
+      maxWidth={500}
+      mx="auto"
+    >
+      <Label mb={1} htmlFor="restaurantName">
+        Nome
+      </Label>
+      <Input
+        id="restaurantName"
+        name="namrestaurantNamee"
+        value={restaurantName}
+        onChange={(e) => setRestaurantName(e.target.value)}
+      />
+      <Flex justifyContent="flex-end">
+        <Button
+          my={3}
+          variant="primary"
+          onClick={async () => {
+            try {
+              const token = await getTokenSilently();
+              await request
+                .post(`${BASE_URL}/restaurants`)
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                  name: restaurantName,
+                });
+              mutate('/restaurants');
+              setRestaurantName('');
+            } catch (err) {
+              alert(err);
+            }
+          }}
+        >
+          Aggiungi ristorante
+        </Button>
+      </Flex>
+    </Box>
+  );
+}
+
+export default AddRestaurant;
